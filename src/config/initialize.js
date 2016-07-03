3dfed0e5b9b8aecf41be5468d2d802d825eb0a70
Leo Adelstein <leoadelstein@gmail.com>
2016-07-02 20:25:32 -0700

    allowing cors

--- src/config/initialize.js
+++ src/config/initialize.js
@@ -1,7 +1,16 @@
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const cors = ((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, cache-control');
+  next();
+});
+
 const initialize = (app) => {
+  app.use(cors);
   app.use(morgan('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
