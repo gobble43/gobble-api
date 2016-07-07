bfdbe1c1ec4d4fd7039aeb82b0a59fc4edce51a9
Jinsoo Cha <jinscha@indiana.edu>
2016-07-06 17:16:02 -0700

    add a route to get product reviews

--- src/routes/product-routes.js
+++ src/routes/product-routes.js
@@ -1,6 +1,7 @@
 const fetch = require('isomorphic-fetch');
 const { checkStatus, parseJSON, handleError } = require('./../lib/fetch-utils');
 const gobbleProductUrl = process.env.GOBBLE_PRODUCT_URL;
+const gobbleDB = process.env.GOBBLE_DB_URL;
 
 const routeProductAPI = (app) => {
   app.post('/product/getRecommendation', (req, res) => {
@@ -23,6 +24,21 @@ const routeProductAPI = (app) => {
         res.sendStatus(err.res.status);
       });
   });
+
+  app.get('/product/getReviews', (req, res) => {
+    const upc = req.query.upc;
+    fetch(`${gobbleDB}/db/getProductReviews?upc=${upc}`)
+      .then(checkStatus)
+      .then(parseJSON)
+      .then(data => {
+        res.status(200).send(data);
+      })
+      .catch(err => {
+        handleError(err);
+        res.sendStatus(err.res.status);
+      });
+  });
 };
+
 
 module.exports = routeProductAPI;
